30d498a5cce13e81d52aa6f93b6c90df85a7d27c
AkiraSpace21 <jaypaygude2@gmail.com>
2025-07-16 16:10:05 +0530

    set and reset bit.cpp

---
+++ set and reset bit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=10;
+    
+    int to_be_set;
+    int to_be_reset;
+    cout<<"enter bit to be set: \n";
+    cin>>to_be_set;
+    cout<<"enter bit to be reset: \n";
+    cin>>to_be_reset;
+    int set=a | (2<<to_be_set);
+    int reset= a|(-(2<<to_be_set));
+    cout<<reset<<endl;
+    cout<<set<<endl;
+return 0;
+}
